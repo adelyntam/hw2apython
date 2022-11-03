db4f404531a2a9f8d968177e2ac8fe3fe1074ced
Adelyn Tam <52673806+adelyntam@users.noreply.github.com>
2022-11-02 22:34:57 -0700

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,9 +1,6 @@
 def sort_dictionary(dictionary):
-        dictionary = sorted(dictionary.items(), key=lambda x:x[1][1])
-        print(dictionary.values())
-        return dictionary
-
-def main():
-    a = {'Tom' : (5464512, 24) , 'Sara' : (5446987, 32) , 'Mary' : (1557896, 20)}
-    print(sort_dictionary(a))
-main()
+        temp = []
+        dictionary = dict(sorted(dictionary.items(), key=lambda x:x[1][1]))
+        for key in dictionary:
+                temp.append((key, dictionary[key][0]))
+        return temp
