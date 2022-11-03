4437eadf37c2a52cb48f7dec4ea4d57667abfc58
Adelyn Tam <52673806+adelyntam@users.noreply.github.com>
2022-11-02 17:14:48 -0700

    Add files via upload

--- rev.py
+++ rev.py
@@ -1,5 +1,6 @@
-def reverse_list(list)
+def reverse_list(list):
 	a = []
 	for i in list:
 		a.insert(0, i)
 	return a;
+
--- sort.py
+++ sort.py
@@ -1,6 +1,9 @@
-def sort_dictionary(dictionary)
-	sorted(dictionary.age())
-	a={}
-	for k in dictionary:
-		a[k] = k[0] 
-	return a
+def sort_dictionary(dictionary):
+        dictionary = sorted(dictionary.items(), key=lambda x:x[1][1])
+        print(dictionary.values())
+        return dictionary
+
+def main():
+    a = {'Tom' : (5464512, 24) , 'Sara' : (5446987, 32) , 'Mary' : (1557896, 20)}
+    print(sort_dictionary(a))
+main()
