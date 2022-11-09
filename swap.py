c2f36636e6904661844897575ac40faca0fbc437
Adelyn Tam <52673806+adelyntam@users.noreply.github.com>
2022-11-09 12:59:33 -0800

    Update swap.py

--- swap.py
+++ swap.py
@@ -1,6 +1,6 @@
 def swap_list(list):
 	i = 0
-	for x in list
+	for x in list:
 		i+=1
 	middleInd = (float(i-1)/2)
-	list[middleInd], list[len-1] = list[len-1], list[middleInd]	
+	list[middleInd], list[i-1] = list[i-1], list[middleInd]	
